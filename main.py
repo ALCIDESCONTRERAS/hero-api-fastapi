159286b57ee0fce672f90426faeb1710eacaf5d5
Alcides <alcidescon@gmail.com>
2025-07-18 09:49:37 +0200

    requirements.txt actualizado

--- main.py
+++ main.py
@@ -14,3 +14,8 @@ def on_startup():
     
 app.include_router(router_heroes)
 app.include_router(router_user)
+
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
